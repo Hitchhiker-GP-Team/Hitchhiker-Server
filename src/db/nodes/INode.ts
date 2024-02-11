99a4c7d98c65583c6707326f86026c2583dba30a
alikashlan1 <76254631+modric10260@users.noreply.github.com>
2024-02-11 22:11:53 +0200

    delete INode

--- src/db/nodes/INode.ts
+++
@@ -1,8 +0,0 @@
-import { dbDriver } from "../dbConnection.js";
-export interface INode<Entity> {
-  //const driver:Driver = dbDriver;
-  create(entity: Entity): boolean;
-  fetch(primaryKey: string): Entity;
-  update(entity: Entity): boolean;
-  delete(primaryKey: string): boolean;
-}
