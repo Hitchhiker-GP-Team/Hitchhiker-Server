634bc3b44e31557f9fafbed3c7cd6d137fdfca53
Ahmed Kandeel <1kandeel1@gmail.com>
2024-02-12 01:20:37 +0200

    clean index

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,6 @@ import { Category } from "./entities/Category.js";
 async function main() {
   await dbStart();
   await startServer();
-  //const Sea: Category = { name: "Sea" };
-  //await DbHelper.CategoryNode.create(Sea);
-
-  await DbHelper.CategoryNode.fetchAllName();
-
-  await DbHelper.CategoryNode.linkChild("Money", "Burger");
-
-  //await DbHelper.CategoryNode.delete("Ocean");
-
-  await DbHelper.CategoryNode.fetchAllName();
-
-  //await DbHelper.CategoryNode.fetch("Food");
 }
 
 main();
