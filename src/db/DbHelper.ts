678a91541617eefb4ee7204d1c85ef7f4c53aee3
Ali <emadalioo27@gmail.com>
2024-02-13 02:12:41 +0200

    alawy

--- src/db/DbHelper.ts
+++ src/db/DbHelper.ts
@@ -1,7 +1,10 @@
 import { CategoryNode } from "./nodes/CategoryNode.js";
 import { PostNode } from "./nodes/PostNode.js";
+import { ReviewNode } from "./nodes/ReviewNode.js";
 
 export class DbHelper {
   public static PostNode: PostNode = new PostNode();
   public static CategoryNode: CategoryNode = new CategoryNode();
+  public static ReviewNode: ReviewNode = new ReviewNode();
+
 }
