55856e31535c794b130d920224fb51877b8e095b
alikashlan1 <76254631+modric10260@users.noreply.github.com>
2024-02-16 21:11:23 +0200

    dbhelper journeynode import

--- src/db/DbHelper.ts
+++ src/db/DbHelper.ts
@@ -1,10 +1,12 @@
 import { CategoryNode } from "./nodes/CategoryNode.js";
 import { PostNode } from "./nodes/PostNode.js";
 import { ReviewNode } from "./nodes/ReviewNode.js";
+import { JourneyNode } from "./nodes/JourneyNode.js";
 
 export class DbHelper {
   public static PostNode: PostNode = new PostNode();
   public static CategoryNode: CategoryNode = new CategoryNode();
   public static ReviewNode: ReviewNode = new ReviewNode();
+  public static JourneyNode: JourneyNode = new JourneyNode();
 
 }
