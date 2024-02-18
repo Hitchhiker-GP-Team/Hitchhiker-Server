54292f7a36662d4eac9c31832099c3d0e6967602
Ali <emadalioo27@gmail.com>
2024-02-18 21:44:40 +0200

    FETCH USER PROFILE

--- src/db/DbHelper.ts
+++ src/db/DbHelper.ts
@@ -1,10 +1,12 @@
 import { CategoryNode } from "./nodes/CategoryNode.js";
 import { PostNode } from "./nodes/PostNode.js";
 import { ReviewNode } from "./nodes/ReviewNode.js";
+import { UserNode } from "./nodes/UserNode.js";
 
 export class DbHelper {
   public static PostNode: PostNode = new PostNode();
   public static CategoryNode: CategoryNode = new CategoryNode();
   public static ReviewNode: ReviewNode = new ReviewNode();
+  public static UserNode: UserNode = new UserNode();
 
 }
