eb841b858481045cfd0387bb0705b39d5f09bbec
Ahmed Kandeel <1kandeel1@gmail.com>
2024-03-09 17:33:28 +0200

    remove unused lines at index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { startServer } from "./api/appServer.js";
-import { DbHelper } from "./db/DbHelper.js";
 import { dbStart } from "./db/dbConnection.js";
-import { Category } from "./entities/Category.js";
 
 async function main() {
   await dbStart();
